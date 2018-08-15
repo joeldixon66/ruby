a21df8c790975233d28d07af9fef49a1baa3dea7
joel <joel@jd53.com>
2018-08-15 13:03:30 +1000

    Openssl

--- 2.3.3/init_container.sh
+++ 2.3.3/init_container.sh
@@ -10,7 +10,7 @@ A P P   S E R V I C E   O N   L I N U X
 
 Documentation: http://aka.ms/webapp-linux
 Ruby quickstart: https://aka.ms/ruby-qs
-ImageMagick libssl-dev
+ImageMagick openssl
 
 EOL
 cat /etc/motd

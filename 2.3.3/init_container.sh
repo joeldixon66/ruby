a6239981fcc2086e1fe2b1fb27ca6c1f1b620537
joel <joel@jd53.com>
2018-08-15 12:28:31 +1000

    Libssl

--- 2.3.3/init_container.sh
+++ 2.3.3/init_container.sh
@@ -10,7 +10,7 @@ A P P   S E R V I C E   O N   L I N U X
 
 Documentation: http://aka.ms/webapp-linux
 Ruby quickstart: https://aka.ms/ruby-qs
-ImageMagick installing puma
+ImageMagick libssl1.0.2
 
 EOL
 cat /etc/motd

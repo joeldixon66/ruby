703fd7d368867a315d5922754880877811fd7f9b
joel <joel@jd53.com>
2018-08-16 18:05:35 +1000

    jesse-backports sources

--- 2.3.3/init_container.sh
+++ 2.3.3/init_container.sh
@@ -10,7 +10,7 @@ A P P   S E R V I C E   O N   L I N U X
 
 Documentation: http://aka.ms/webapp-linux
 Ruby quickstart: https://aka.ms/ruby-qs
-ImageMagick jesse-backport
+ImageMagick jesse-backport with sources.list
 
 EOL
 cat /etc/motd

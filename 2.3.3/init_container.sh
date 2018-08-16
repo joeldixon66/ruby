3b1d74d5accba5676a90aa3183852bf8120a8e97
joel <joel@jd53.com>
2018-08-17 09:30:02 +1000

    libssl-dev

--- 2.3.3/init_container.sh
+++ 2.3.3/init_container.sh
@@ -10,7 +10,7 @@ A P P   S E R V I C E   O N   L I N U X
 
 Documentation: http://aka.ms/webapp-linux
 Ruby quickstart: https://aka.ms/ruby-qs
-ImageMagick jesse-backport with sources.list
+ImageMagick libssl-dev
 
 EOL
 cat /etc/motd

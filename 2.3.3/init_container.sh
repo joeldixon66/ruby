fe784030e7f7b0b0c748b5924cc59a2c376cbda7
joel <joel@jd53.com>
2018-08-17 15:05:30 +1000

    ruby version 2.4.4

--- 2.3.3/init_container.sh
+++ 2.3.3/init_container.sh
@@ -10,7 +10,7 @@ A P P   S E R V I C E   O N   L I N U X
 
 Documentation: http://aka.ms/webapp-linux
 Ruby quickstart: https://aka.ms/ruby-qs
-ImageMagick 2.4.4
+ImageMagick 2.4.4 ruby
 
 EOL
 cat /etc/motd

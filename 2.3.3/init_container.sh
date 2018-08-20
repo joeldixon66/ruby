46765c2ced188d77e877d06ec73445d34e2cee04
joel <joel@jd53.com>
2018-08-20 15:00:37 +1000

    Trying 2.4.0

--- 2.3.3/init_container.sh
+++ 2.3.3/init_container.sh
@@ -10,7 +10,7 @@ A P P   S E R V I C E   O N   L I N U X
 
 Documentation: http://aka.ms/webapp-linux
 Ruby quickstart: https://aka.ms/ruby-qs
-ImageMagick bundle check
+ImageMagick ruby 2.4.0
 
 EOL
 cat /etc/motd
@@ -22,7 +22,7 @@ eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
 
 eval "$(rbenv init -)"
 echo "LD_LIBRARY_PATH - $LD_LIBRARY_PATH"
-rbenv global 2.4.4
+rbenv global 2.4.0
 gem install bundler
 /opt/startup.sh "$@"
 

8d625673b1ed26a26ab0ce2b85c9228ebb577fe4
joel <joel@jd53.com>
2018-08-23 10:34:51 +1000

    LD_LIBRARY_PATH 2

--- 2.3.3/init_container.sh
+++ 2.3.3/init_container.sh
@@ -10,7 +10,7 @@ A P P   S E R V I C E   O N   L I N U X
 
 Documentation: http://aka.ms/webapp-linux
 Ruby quickstart: https://aka.ms/ruby-qs
-ImageMagick ld_library_path
+ImageMagick ld_library_path 2
 
 EOL
 cat /etc/motd
